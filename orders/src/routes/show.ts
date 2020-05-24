aa870016860e2dd9b5ee04438d86228231d53460
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2020-05-24 18:15:59 +0200

    fix orders service

---
+++ orders/src/routes/show.ts
@@ -0,0 +1,15 @@
+import express, { Router, Request, Response } from 'express';
+// import { orders } from '../model/orders';
+
+const router: Router = express.Router();
+
+router.get(
+  '/api/orders/:orderId',
+  async (req: Request, res: Response) => {
+    // const tickets = await Ticket.find({});
+    // res.status(200).send(tickets); 
+    res.send({});
+  }
+);
+
+export { router as showOrderRouter };
