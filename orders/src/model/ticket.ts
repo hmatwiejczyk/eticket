2ebf49cb1b20b31d1948a4cb76e1be0a3e1ec7f7
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2020-05-27 00:44:52 +0200

    orders add version

--- orders/src/model/ticket.ts
+++ orders/src/model/ticket.ts
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
 import { Order, OrderStatus } from './order';
+import { updateIfCurrentPlugin } from 'mongoose-update-if-current';
 
 interface TicketAttrs {
   id: string;
@@ -10,50 +11,58 @@ interface TicketAttrs {
 export interface TicketDoc extends mongoose.Document {
   title: string;
   price: number;
-  isReserved(): Promise<boolean> 
+  version: number;
+  isReserved(): Promise<boolean>;
 }
 
 interface TicketModel extends mongoose.Model<TicketDoc> {
   build(attrs: TicketAttrs): TicketDoc;
 }
 
-const ticketSchema = new mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const ticketSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    price: {
+      type: Number,
+      required: true,
+      min: 0,
+    },
   },
-  price: {
-    type: Number,
-    required: true,
-    min: 0
-  },
-}, {
-  toJSON: {
-    transform(doc, ret) {
-      ret.id = ret._id;
-      delete ret._id;
-    }
+  {
+    toJSON: {
+      transform(doc, ret) {
+        ret.id = ret._id;
+        delete ret._id;
+      },
+    },
   }
-});
+);
 
-ticketSchema.statics.build = (attrs: TicketAttrs) => new Ticket({
-  _id: attrs.id,
-  title: attrs.title,
-  price: attrs.price
-});
-ticketSchema.methods.isReserved = async function() {
+ticketSchema.statics.build = (attrs: TicketAttrs) =>
+  new Ticket({
+    _id: attrs.id,
+    title: attrs.title,
+    price: attrs.price,
+  });
+ticketSchema.methods.isReserved = async function () {
   const existingOrder = await Order.findOne({
     ticket: this,
     status: {
       $in: [
         OrderStatus.Created,
         OrderStatus.AwaitingPayment,
-        OrderStatus.Complete
-      ]
-    }
+        OrderStatus.Complete,
+      ],
+    },
   });
   return !!existingOrder;
-}
+};
+
+ticketSchema.set('versionKey', 'version');
+ticketSchema.plugin(updateIfCurrentPlugin);
 
 const Ticket = mongoose.model<TicketDoc, TicketModel>('Ticket', ticketSchema);
 
