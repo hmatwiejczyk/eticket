bf2e0539616c4386040f8915e944b24f7b62c40a
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2020-05-26 22:58:40 +0200

    implement listeners

--- orders/src/index.ts
+++ orders/src/index.ts
@@ -1,6 +1,8 @@
 import { natsWrapper } from './nats-wrapper';
 import mongoose from 'mongoose';
 import { app } from './app';
+import { TicketCreatedListener } from './events/listeners/ticket-created-listener';
+import { TicketUpdatedListener } from './events/listeners/ticket-updated-listener';
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
@@ -31,6 +33,9 @@ const start = async () => {
     });
     process.on('SIGINT', () => natsWrapper.client.close());
     process.on('SIGTERM', () => natsWrapper.client.close());
+    
+    new TicketCreatedListener(natsWrapper.client).listen();
+    new TicketUpdatedListener(natsWrapper.client).listen();
 
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
