7e1c97be2633759e0fa6d827d6e14008833592e6
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2020-06-14 23:57:02 +0200

    add expiration queue

--- expiration/src/events/listeners/order-created-listener.ts
+++ expiration/src/events/listeners/order-created-listener.ts
@@ -1,9 +1,15 @@
 import { Message } from 'node-nats-streaming';
 import { queueGroupName } from './queue-group-name';
 import { Listener, OrderCreatedEvent, Subjects } from '@hmtickets/common';
+import { expirationQueue } from '../../queues/expiration-queue';
 
 export class OrderCreatedListener extends Listener<OrderCreatedEvent> {
   subject: Subjects.OrderCreated = Subjects.OrderCreated;
   queueGroupName = queueGroupName;
-  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {}
+  async onMessage(data: OrderCreatedEvent['data'], msg: Message) {
+    await expirationQueue.add({
+      orderId: data.id,
+    });
+    msg.ack();
+  }
 }
