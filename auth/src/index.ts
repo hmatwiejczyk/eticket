772366e1bd69559448eeb8bbd06e6ae65a8f620a
Hubert Matwiejczyk <hmatwiejczyk@gmail.com>
2020-06-23 23:24:08 +0200

    add startup message

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -3,6 +3,7 @@ import { app } from './app';
 const PORT = process.env.PORT || 3000;
 
 const start = async () => {
+  console.log('Starting up...');
   if (!process.env.JWT_KEY) {
     throw new Error('JWT_KEY must be defined');
   }
